c2210db4f60743e326f56103a2ba5184324dcfc6
casandrabenpor <casandra211@gmail.com>
2023-08-02 15:31:23 +0200

    animation

--- src/components/tecnologies.jsx
+++ src/components/tecnologies.jsx
@@ -10,24 +10,25 @@ import Github from "../assets/github.svg";
 import Mysql from "../assets/mysql.svg";
 import Mongo from "../assets/mongodb.svg";
 import Php from "../assets/php.svg"
+import "../styles/animation.scss";
 
 
 export const Tecnologies = () => {
     return (
         <div>
             <h3 className="title_tecnologies">TECNOLOGÍAS</h3>
-            <div className="icons">
-                <img src={Bootstrap} alt="bootstrap"></img>
-                <img src={Html} alt="html5"></img>
-                <img src={Css} alt="css"></img>
-                <img src={Sass} alt="sass"></img>
-                <img src={Js} alt="Javascript"></img>
-                <img src={React} alt="react"></img>
-                <img src={Redux} alt="redux"></img>
-                <img src={Github} alt="GitHub"></img>
-                <img src={Mysql} alt="MySQL"></img>
-                <img src={Mongo} alt="Mongo"></img>
-                <img src={Php} alt="Php"></img>
+            <div className=" icons">
+                <img src={Bootstrap} alt="bootstrap" className="btn-a heartbeat" />
+                <img src={Html} alt="html5" className="btn-a heartbeat" />
+                <img src={Css} alt="css" className="btn-a heartbeat" />
+                <img src={Sass} alt="sass" className="btn-a heartbeat" />
+                <img src={Js} alt="Javascript" className="btn-a heartbeat" />
+                <img src={React} alt="react" className="btn-a heartbeat" />
+                <img src={Redux} alt="redux" className="btn-a heartbeat" />
+                <img src={Github} alt="GitHub" className="btn-a heartbeat" />
+                <img src={Mysql} alt="MySQL" className="btn-a heartbeat" />
+                <img src={Mongo} alt="Mongo" className="btn-a heartbeat" />
+                <img src={Php} alt="Php" className="btn-a heartbeat" />
             </div>
         </div>
     )
