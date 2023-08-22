8e761c823aaf09538e8b8bbd4e9f400d55d1335f
casandrabenpor <casandra211@gmail.com>
2023-08-22 12:39:06 +0200

    about text

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -14,6 +14,16 @@ export const About = () => {
             Estoy constantemente en busca de nuevas oportunidades para aprender y poner en práctica mis conocimientos.
           </p>
           <p>
+            Actualmente, me encuentro realizando prácticas donde he tenido la oportunidad de adquirir
+            experiencia en diversas tecnologías, entre las que destacan <b>React Redux, HTML5, CSS,
+              Sass, JavaScript, MySQL, MongoDB y PHP.</b>
+            <p>
+              Durante mis prácticas, he participado en proyectos significativos, como el desarrollo de una
+              página web e-commerce , una aplicación de edición de fotos y, en la actualidad, estamos
+              trabajando en un sitio web para un hotel y un panel de control.
+            </p>
+          </p>
+          <p>
             Me interesa especialmente la maquetación y todo lo relacionado con el front-end, ya que considero que es un área fundamental para crear interfaces de usuario atractivas y accesibles. Sin embargo, también estoy abierta a explorar otros campos dentro del desarrollo web y ampliar mis habilidades en áreas como el back-end, bases de datos y la creación de aplicaciones web completas.
           </p>
           <p>
